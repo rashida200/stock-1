1b5ae20f431629b1716ddaa42b47ba40e850b72e
Hassan AIT JABLI <hassanjebli2002@gmail.com>
2024-12-08 21:52:18 +0100

    update

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -3,17 +3,6 @@
     <a class="nav-link mb-2 rounded {{ request()->is('admin') || request()->is('manager') || request()->is('cashier') ? 'active' : '' }}" href="/home">
       <i class="fas fa-home me-2"></i>Dashboard
     </a>
-
-    <!-- Inventory Link: Cashier Access -->
-    {{-- @if (auth()->user()->type === 'cashier' ||  auth()->user()->type === 'admin')
-      <a class="nav-link mb-2 rounded {{ request()->is('inventory*') ? 'active' : '' }}" href="#">
-        <i class="fas fa-box me-2"></i>Inventory
-      </a>
-    @endif --}}
-
-
-
-
     <!-- Fournisseurs Link: Admin Access -->
     @if (auth()->user()->type === 'admin')
       <a class="nav-link mb-2 rounded {{ request()->is('admin/fournisseurs*') ? 'active' : '' }}" href="{{ route('admin.fournisseurs') }}">
