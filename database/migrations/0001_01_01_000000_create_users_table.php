f29a841f5f68d05942c81aeed37b75dd69c4b471
rashida <rchamali197@gmail.com>
2024-12-22 15:18:03 +0100

    rename cashier to commercial

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('type', ['admin', 'manager', 'cashier'])->default('cashier');
+            $table->enum('type', ['admin', 'manager', 'commercial']);
             $table->rememberToken();
             $table->timestamps();
         });
